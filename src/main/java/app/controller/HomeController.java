e943a02b0d7750200577c8fbd5adf8c46cad4067
EMS1S-PC\Emsss777 <emil.p.ganchev@gmail.com>
2025-09-29 16:29:29 +0200

    feat: add getHomePage

---
+++ src/main/java/app/controller/HomeController.java
@@ -0,0 +1,21 @@
+package app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import static app.util.UrlPaths.URL_HOME;
+import static app.util.Views.VIEW_HOME;
+
+@Controller
+public class HomeController {
+
+    @GetMapping(URL_HOME)
+    public ModelAndView getHomePage() {
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName(VIEW_HOME);
+
+        return modelAndView;
+    }
+}
--- src/main/java/app/util/UrlPaths.java
+++ src/main/java/app/util/UrlPaths.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 public final class UrlPaths {
 
     public static final String URL_ROOT = "/";
+    public static final String URL_HOME = "/home";
     public static final String URL_LOGIN = "/login";
     public static final String URL_REGISTER = "/register";
     public static final String URL_CATALOG = "/catalog";
