8037870092ba2a0e45d78bbe3c66d34717374ba9
EMS1S-PC\Emsss777 <emil.p.ganchev@gmail.com>
2025-10-31 18:43:40 +0100

    chore: updating ArtistController to send DTOs

--- src/main/java/app/web/controller/ArtistController.java
+++ src/main/java/app/web/controller/ArtistController.java
@@ -1,10 +1,14 @@
 package app.web.controller;
 
+import app.mapper.ArtistMapper;
+import app.model.dto.ArtistDTO;
 import app.service.ArtistService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 import static app.util.ModelAttributes.MODEL_LATEST_ARTISTS;
 import static app.util.UrlPaths.*;
@@ -19,9 +23,11 @@ public class ArtistController {
     @GetMapping(URL_ADMIN_ARTISTS + URL_ADD)
     public ModelAndView getAddArtistPage() {
 
+        List<ArtistDTO> latestArtists = ArtistMapper.toDTOList(artistService.getLatestArtists(4));
+
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName(VIEW_ADMIN_ADD_ARTIST);
-        modelAndView.addObject(MODEL_LATEST_ARTISTS, artistService.getLatestArtists(4));
+        modelAndView.addObject(MODEL_LATEST_ARTISTS, latestArtists);
 
         return modelAndView;
     }
