e763aee5da1239fbecf5dd0f0cd5c4519012d306
EMS1S-PC\Emsss777 <emil.p.ganchev@gmail.com>
2025-11-27 14:57:32 +0100

    refactor(redis): clean up old cacheConfiguration and consolidate all cache settings into RedisCacheManager

--- src/main/java/app/config/RedisConfig.java
+++ src/main/java/app/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package app.config;
 
+import app.model.dto.AlbumDTO;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
@@ -7,28 +8,53 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 @EnableCaching
 public class RedisConfig {
 
     @Bean
-    public RedisCacheConfiguration cacheConfiguration() {
+    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
 
         ObjectMapper mapper = new ObjectMapper()
                 .registerModule(new JavaTimeModule())
                 .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
 
-        GenericJackson2JsonRedisSerializer serializer =
+        GenericJackson2JsonRedisSerializer genericSerializer =
                 new GenericJackson2JsonRedisSerializer(mapper);
 
-        return RedisCacheConfiguration.defaultCacheConfig()
-                .serializeValuesWith(
-                        RedisSerializationContext
-                                .SerializationPair
-                                .fromSerializer(serializer)
-                );
+        Jackson2JsonRedisSerializer<AlbumDTO> albumDetailsSerializer =
+                new Jackson2JsonRedisSerializer<>(mapper, AlbumDTO.class);
+
+        RedisCacheConfiguration defaultConfig =
+                RedisCacheConfiguration.defaultCacheConfig()
+                        .serializeValuesWith(
+                                RedisSerializationContext.SerializationPair.fromSerializer(genericSerializer)
+                        );
+
+        Map<String, RedisCacheConfiguration> cacheConfigs = new HashMap<>();
+
+        cacheConfigs.put("albumDetails",
+                defaultConfig.serializeValuesWith(
+                        RedisSerializationContext.SerializationPair.fromSerializer(albumDetailsSerializer)
+                )
+        );
+
+        cacheConfigs.put("artists", defaultConfig);
+        
+        cacheConfigs.put("albumCatalog", defaultConfig);
+
+        return RedisCacheManager.builder(connectionFactory)
+                .cacheDefaults(defaultConfig)
+                .withInitialCacheConfigurations(cacheConfigs)
+                .build();
     }
 }
